ccf17d9353c9b23f4072a72be8c5a08bcd81f536
chunlchan <chunliangchan@gmail.com>
2026-01-07 16:58:59 -0600

    Demo of item blocking and shuffling

--- src/views/IntroView.vue
+++ src/views/IntroView.vue
@@ -2,8 +2,13 @@
     <v-container>
         <h1>Introduction</h1>
         <p>
-      Welcome to our study. Please press the "Next" button below to proceed to the consent form and audio setup.
+            Welcome to our study. Please press the "Next" button below to proceed to the consent form and audio setup.
         </p>
+        <v-data-table multi-sort="" items-per-page="50" density="compact" v-if="participantStore.list !== null" :items="participantStore.list">
+            <template v-slot:[`item.t2dialog`]="{ value }">
+                <p class="text-truncate" style="max-width: 100px;">{{value }}</p>
+            </template>
+        </v-data-table>
         <v-btn @click="next" class="mt-4" color="primary">Next <v-icon>chevron_right</v-icon></v-btn>
     </v-container>
 </template>
@@ -48,6 +53,9 @@ onMounted(async () => {
     } else {
         nextDisabled.value = false;
     }
+
+    //clear participantStore.list
+    participantStore.list = null
 })
 
 const next = async () => {
@@ -71,7 +79,8 @@ const next = async () => {
         })
         return;
     }
-    router.push("/consent");
+
+    //router.push("/consent");
 }
 
 const loadLists = (listFile) => {
@@ -89,48 +98,99 @@ const loadLists = (listFile) => {
     })
 }
 
-function createBlocks(list){
+function createBlocks(list) {
     let blockedList = [];
     let groupedList = groupBy(list, 'itemType');
-    groupedList.presentabsent = shuffle(groupedList.presentabsent);
-    groupedList.NPAsubjobj = shuffle(groupedList.NPAsubjobj);
+    
+    let groupedPa = groupBy(groupedList.presentabsent, "item");
     groupedList.filler = shuffle(groupedList.filler);
-    let index16 = 0;
-    let index8 = 0
-    for(index16=0;index16<16;index16+=2){
-        blockedList.push(shuffle([groupedList.presentabsent[index16], groupedList.presentabsent[index16 + 1], groupedList.NPAsubjobj[index8], groupedList.filler[index8]]));
-        index8 +=1;
+    let index = 0;
+    let fillerIndex = 0;
+    for (index = 0; index < 4; index += 1) {
+
+        //select presentabsent items
+        let block = [];
+        Object.values(groupedPa).forEach(item => {
+            block.push(item[index % 4]);
+        });
+        //blockedList.push(selectedPas)
+
+        //select NPAsubject items
+        if (index % 4 === 0 || index % 4 === 1) {
+            block.push(
+                groupedList.NPAsubjobj.filter((el) => el.item == "lily")[index % 2],
+                groupedList.NPAsubjobj.filter((el) => el.item == "manny")[index % 2],
+            );
+        } else {
+            block.push(
+                groupedList.NPAsubjobj.filter((el) => el.item == "mary")[index % 2],
+                groupedList.NPAsubjobj.filter((el) => el.item == "molly")[index % 2],
+            );
+        }
+
+
+        //select fillters
+        block.push(
+            groupedList.filler[fillerIndex],
+            groupedList.filler[fillerIndex + 1],
+        );
+        fillerIndex += 2;
+
+        blockedList.push(block)
+
+    }
+    
+    //shuffle blocks
+    for(let b=0;b<blockedList.length;b++){
+        blockedList[b] = shuffle(blockedList[b]);
+        console.log(blockedList[b]);
+    }
+    
+    //check for overlapping items between blocks
+    for(let b=0;b<blockedList.length - 1;b++){
+        let currentBlock = blockedList[b]; 
+        let nextBlock = blockedList[b + 1]; 
+        while(currentBlock[currentBlock.length - 1].item == nextBlock[0].item){
+            console.log("overlaps found, reshuffling...");
+            blockedList[b] = shuffle(blockedList[b]);
+        }
     }
     blockedList = blockedList.flat()
-    //console.log(blockedList)
     return blockedList
-
 }
 
 function groupBy(arr, key) {
-  return arr.reduce((result, currentValue) => {
-    // Get the value of the key for the current object
-    const groupKey = currentValue[key];
+    let groupArr = arr.reduce((result, currentValue) => {
+        // Get the value of the key for the current object
+        const groupKey = currentValue[key];
 
-    // If the key doesn't exist in the result object, create an empty array
-    if (!result[groupKey]) {
-      result[groupKey] = [];
+        // If the key doesn't exist in the result object, create an empty array
+        if (!result[groupKey]) {
+            result[groupKey] = [];
+        }
+
+        // Push the current object to the corresponding group
+        result[groupKey].push(currentValue);
+
+        return result;
+    }, {});
+
+    Object.keys(groupArr).forEach(key => {
+        groupArr[key] = shuffle(groupArr[key]);
+    })
+
+    return groupArr;
+}
+
+function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
     }
-
-    // Push the current object to the corresponding group
-    result[groupKey].push(currentValue);
-
-    return result;
-  }, {});
-}
-
-function shuffle(array) {var m = array.length,t,i;
-while (m) {i = Math.floor(Math.random() * m--);
-t = array[m];
-array[m] = array[i];
-array[i] = t;
-}
-return array;
+    return array;
 }
 
 </script>
