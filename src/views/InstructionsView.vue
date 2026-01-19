4f4bcc6614c07cdf865b7556087c21e58b8683d2
rstanhope <rebekahmstanhope@gmail.com>
2026-01-19 09:56:37 -0600

    change instructions to task 1 instructions

--- src/views/InstructionsView.vue
+++ src/views/InstructionsView.vue
@@ -2,6 +2,7 @@
     <v-container>
         <h1>Instructions</h1>  <!-- t2 instructions -->
         <div class="d-flex flex-column">
+            <!--
             <p style="padding-bottom:10px"> In this experiment, you will listen to sentences and make judgments about whether
             or not the speaker is surprised, and if so, what they are surprised about, based on the way they said a sentence. </p>
 
@@ -11,6 +12,15 @@
             also be asked to choose a response to the sentence you just heard. We want to know which response you think fits best, 
             based on the way the speaker said the sentence. For each part of this task, we want to know your first impression,
             so you should respond quickly. </p>
+            -->
+
+            <!-- t1 instructions -->
+            <p style="padding-bottom:10px"> In this experiment, you will listen to sentences and choose the question you think
+            the speaker was responding to, based on the way they said the sentence. </p>
+
+            <p style="padding-bottom:10px"> On each trial, you hear a recorded sentence. After you listen to the recording,
+            you will see two questions written on the screen. You will click a button to indicate which question you think the
+            speaker was answering. We want to know your first impression, so you should respond quickly. </p>
 
             <p style="padding-bottom:10px"> After each trial, you will see a number on the screen. You will count up aloud by 2s starting
             from the number on the screen until the number disappears. For example, if you see 18, you would say "20, 22, 24..." </p>
